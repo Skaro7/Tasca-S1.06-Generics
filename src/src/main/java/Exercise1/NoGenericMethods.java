1df289a8d58df1270526063017359488f49dbc8f
Skaro7 <marc_tau72@hotmail.com>
2026-01-14 11:36:22 +0100

    feat: add NoGenericMethods class

---
+++ src/src/main/java/Exercise1/NoGenericMethods.java
@@ -0,0 +1,31 @@
+package Exercise1;
+
+public class NoGenericMethods {
+    private Object element1;
+    private Object element2;
+    private Object element3;
+
+    public NoGenericMethods(Object element1, Object element2, Object element3) {
+        this.element1 = element1;
+        this.element2 = element2;
+        this.element3 = element3;
+    }
+
+    public Object getElement1() {
+        return element1;
+    }
+
+    public Object getElement2() {
+        return element2;
+    }
+
+    public Object getElement3() {
+        return element3;
+    }
+
+    public void printElements() {
+        System.out.println("Element 1: " + element1);
+        System.out.println("Element 2: " + element2);
+        System.out.println("Element 3: " + element3);
+    }
+}
